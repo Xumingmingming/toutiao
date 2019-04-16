dff0943e8d8903ea84e7bf1461c3edefe948f923
Xumingmingming <393782036@qq.com>
2019-04-16 17:30:42 +0800

    这里取消了没用的包

--- src/test/java/com/nowcoder/service/MessageServiceTest.java
+++ src/test/java/com/nowcoder/service/MessageServiceTest.java
@@ -2,7 +2,6 @@ package com.nowcoder.service;
 
 import com.nowcoder.ToutiaoApplication;
 import com.nowcoder.model.Message;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
